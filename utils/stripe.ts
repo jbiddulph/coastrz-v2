792752866e5875b6d64d85938fe4fe4d6b8a0eb2
John <john.mbiddulph@gmail.com>
2025-04-23 23:45:02 +0100

    clear the Stripe singleton instance when the publishable key changes

--- utils/stripe.ts
+++ utils/stripe.ts
@@ -2,10 +2,20 @@ import { loadStripe } from '@stripe/stripe-js';
 
 // This is a singleton to ensure we only create one instance
 let stripePromise: Promise<any> | null = null;
+let currentKey: string | null = null;
 
 export const getStripe = () => {
-  if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+  const key = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
+  
+  // If the key has changed, clear the singleton
+  if (currentKey && currentKey !== key) {
+    stripePromise = null;
   }
+  
+  if (!stripePromise) {
+    stripePromise = loadStripe(key);
+    currentKey = key;
+  }
+  
   return stripePromise;
 }; 
