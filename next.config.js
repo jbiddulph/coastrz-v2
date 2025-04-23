8037a9dc1d98f3d5d60756d81f10f88407e86106
John <john.mbiddulph@gmail.com>
2025-04-23 21:10:25 +0100

    Update Next.js config for standalone build dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: undefined,
+    outputFileTracingIncludes: {
+      '**/*': [
+        'node_modules/**/*.{js,json,node}',
+        '.next/standalone/**/*',
+        '.next/static/**/*',
+      ],
+    },
+  },
   images: {
     remotePatterns: [
       {
