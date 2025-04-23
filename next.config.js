5b28e0cf58a7d0a4966add6d95f91f3f85ea1eba
John <john.mbiddulph@gmail.com>
2025-04-23 21:05:25 +0100

    Update build configuration for standalone output

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  experimental: {
-    outputFileTracingIncludes: {
-      '/**/*': ['./public/**/*']
-    }
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'tjsnzjiouvtzgheyyiac.supabase.co',
-        port: '',
         pathname: '/**',
       },
     ],
