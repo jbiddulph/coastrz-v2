9ec18558e5a9af5f4fefa0a0d88db0db4d58e56f
John <john.mbiddulph@gmail.com>
2025-07-17 17:45:36 +0100

    fixing bugs

--- next.config.js
+++ next.config.js
@@ -12,12 +12,9 @@ const nextConfig = {
   // Disable static generation to prevent useContext errors
   experimental: {
     // Force all pages to be dynamic
-    staticPageGenerationTimeout: 0,
   },
   // Ensure all pages are rendered dynamically
   trailingSlash: false,
-  // Disable static optimization
-  staticPageGenerationTimeout: 0,
 }
 
 module.exports = nextConfig
