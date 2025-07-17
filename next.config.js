56c7082bb54f710c8f6169184a3ce53e440fac05
John <john.mbiddulph@gmail.com>
2025-07-17 20:25:11 +0100

    sorting stuff

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
