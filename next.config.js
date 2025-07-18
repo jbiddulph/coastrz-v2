a1663b0878edfe7528277c0959a5b5e85acec410
John <john.mbiddulph@gmail.com>
2025-07-18 09:13:53 +0100

    getting ready to deploy

--- next.config.js
+++ next.config.js
@@ -1,14 +1,40 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'standalone',
+//   // Disable static generation completely
+//   trailingSlash: false,
+//   images: {
+//     unoptimized: true,
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'qemafehpoknkbejlbksa.supabase.co',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // Disable static generation completely
-  trailingSlash: false,
+  experimental: {
+    outputFileTracingRoot: undefined,
+    outputFileTracingIncludes: {
+      '**/*': [
+        'node_modules/**/*.{js,json,node}',
+        '.next/standalone/**/*',
+        '.next/static/**/*',
+      ],
+    },
+  },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'qemafehpoknkbejlbksa.supabase.co',
+        hostname: 'tjsnzjiouvtzgheyyiac.supabase.co',
         pathname: '/**',
       },
     ],
